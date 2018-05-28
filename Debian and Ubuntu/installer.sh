82cca170b600614dcc91b012778333110a0a7c77
E-Media Arts <39668485+Driz12@users.noreply.github.com>
2018-05-29 00:44:12 +0800

    Update installer.sh

--- Debian and Ubuntu/installer.sh
+++ Debian and Ubuntu/installer.sh
@@ -15,8 +15,8 @@ cd /root
 apt-get update -y
 apt-get install build-essential -y
 #Downloading server files
-wget http://www.softether-download.com/files/softether/v4.27-9667-beta-2018.05.26-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9667-beta-2018.05.26-linux-x64-64bit.tar.gz
-tar zxf softether-vpnserver-v4.27-9667-beta-2018.05.26-linux-x64-64bit.tar.gz
+wget http://www.softether-download.com/files/softether/v4.27-9666-beta-2018.04.21-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
+tar zxf softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
 cd vpnserver
 #Installing server files, Manual input
 clear
@@ -24,13 +24,13 @@ echo  -e "\033[31;7mNOTE: ANSWER 1 AND ENTER THREE TIMES FOR THE COMPILATION TO 
 make
 cd /root
 mv vpnserver /usr/local
-rm -rf softether-vpnserver-v4.27-9667-beta-2018.05.26-linux-x64-64bit.tar.gz
+rm -rf softether-vpnserver-v4.27-9666-beta-2018.04.21-linux-x64-64bit.tar.gz
 cd /usr/local/vpnserver
 chmod 600 *
 chmod 700 vpncmd
 chmod 700 vpnserver
-#Installing Server Command
-wget https://raw.githubusercontent.com/jaysonvelagio/SoftetherAutoInstallMultiPlatform/master/Debian%20and%20Ubuntu/vpn-server.sh --no-check-certificate
+#Installing server command
+wget https://raw.githubusercontent.com/Driz12/SoftetherAutoInstallMultiPlatform/master/Debian%20and%20Ubuntu/vpn-server.sh --no-check-certificate
 mv vpn-server.sh /etc/init.d/vpnserver
 cd /etc/init.d/
 chmod 755 vpnserver
@@ -44,6 +44,5 @@ echo ---------------Commands----------------------
 echo /etc/init.d/vpnserver start - to start
 echo /etc/init.d/vpnserver restart - to restart
 echo /etc/init.d/vpnserver stop - to stop
-echo /usr/local/vpnserver/vpncmd
 echo ---------------------------------------------
-#End of script
+#End Of Script
