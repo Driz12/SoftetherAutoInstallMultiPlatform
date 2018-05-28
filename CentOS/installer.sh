48c216f086ed9650330476b3d5fbcb6ec548abda
E-Media Arts <39668485+Driz12@users.noreply.github.com>
2018-05-29 00:39:15 +0800

    Update installer.sh

--- CentOS/installer.sh
+++ CentOS/installer.sh
@@ -32,7 +32,7 @@ chmod 600 *
 chmod 700 vpncmd
 chmod 700 vpnserver
 #Installing server command
-wget https://raw.githubusercontent.com/jaysonvelagio/SoftetherAutoInstallMultiPlatform/master/Centos/vpn-server.sh --no-check-certificate
+wget https://raw.githubusercontent.com/Driz12/SoftetherAutoInstallMultiPlatform/master/CentOS/vpn-server.sh --no-check-certificate
 mv vpn-server.sh /etc/init.d/vpnserver
 cd /etc/init.d/
 chmod 755 vpnserver
